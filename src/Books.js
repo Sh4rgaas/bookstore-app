3094e7c0b15b5a1d846590aa393e56062d3fe68b
Sh4rgaas <shloosh41@yahoo.com>
2022-05-10 17:11:26 +0300

    some small additions for responsiveness

--- src/Books.js
+++ src/Books.js
@@ -19,8 +19,8 @@ const Books = () => {
 
   return (
     <>
-        <h1 className='font-bold text-center text-4xl py-5'>Books</h1>
-        <section className='grid grid-cols-1 gap-10 px-5 sm:grid-cols-2'>
+        <h1 className='font-bold text-center text-4xl py-5 lg:text-6xl'>Bestsellers</h1>
+        <section className='grid grid-cols-1 gap-10 px-5 pb-20 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
             {books.map((book) => {
                 const {author, book_image, buy_links, description, primary_isbn10, publisher, rank, title} = book
 
